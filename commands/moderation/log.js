284aada859eed9598731695662a5f32a720a0d6c
Daney <daneyyhh64@gmail.com>
2025-11-04 19:25:34 +0530

    Add log command to moderation

---
+++ commands/moderation/log.js
@@ -0,0 +1,8 @@
+// Placeholder for log command
+module.exports = {
+  name: 'log',
+  description: 'Log server/moderation actions to an admin channel',
+  execute(message, args) {
+    // Implementation coming soon
+  },
+};
