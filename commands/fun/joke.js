ba2acb9f463272df29a2b8bbc85b964c360b409e
Daney <daneyyhh64@gmail.com>
2025-11-04 19:04:22 +0530

    Add joke command placeholder
    This file serves as a placeholder for the joke command, which will tell a random joke from various categories once implemented.

---
+++ commands/fun/joke.js
@@ -0,0 +1,10 @@
+// Placeholder for joke command
+// This command tells a random joke from various categories
+
+module.exports = {
+  name: 'joke',
+  description: 'Tells a random joke from various categories',
+  execute(message, args) {
+    // Implementation coming soon
+  },
+};
