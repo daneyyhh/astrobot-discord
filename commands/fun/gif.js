720ed512474d102b0c81408b3a9c0aa23b026a55
Daney <daneyyhh64@gmail.com>
2025-11-04 19:27:35 +0530

    Add gif command to fun

---
+++ commands/fun/gif.js
@@ -0,0 +1,8 @@
+// Placeholder for gif command
+module.exports = {
+  name: 'gif',
+  description: 'Post a gif based on search keyword',
+  execute(message, args) {
+    // Implementation coming soon
+  },
+};
