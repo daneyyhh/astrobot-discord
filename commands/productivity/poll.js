4cae46f9881c7fa14bec9a80407bafedcfe9b3d0
Daney <daneyyhh64@gmail.com>
2025-11-04 19:06:16 +0530

    Add poll command placeholder in productivity folder

---
+++ commands/productivity/poll.js
@@ -0,0 +1,10 @@
+// Placeholder for poll command
+// This command creates a poll with reaction voting
+
+module.exports = {
+  name: 'poll',
+  description: 'Create a poll with reaction voting',
+  execute(message, args) {
+    // Implementation coming soon
+  },
+};
