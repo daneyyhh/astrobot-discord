fe0cd3ce1a9d90664a55fd6c354d76776aef3c00
Daney <daneyyhh64@gmail.com>
2025-11-04 19:29:40 +0530

    Add joinvc command to utility

---
+++ commands/utility/joinvc.js
@@ -0,0 +1,8 @@
+// Placeholder for joinvc command
+module.exports = {
+  name: 'joinvc',
+  description: 'Join the user\'s voice channel',
+  execute(message, args) {
+    // Implementation coming soon
+  },
+};
