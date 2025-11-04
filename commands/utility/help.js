7f0336046f8d8a5ccf0ae98ed00c65400a7b38e3
Daney <daneyyhh64@gmail.com>
2025-11-04 19:07:09 +0530

    Add help command placeholder in utility folder

---
+++ commands/utility/help.js
@@ -0,0 +1,10 @@
+// Placeholder for help command
+// This command displays all available commands
+
+module.exports = {
+  name: 'help',
+  description: 'Display all available commands',
+  execute(message, args) {
+    // Implementation coming soon
+  },
+};
