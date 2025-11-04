e9f0f37a77995a1e2619610d90de94fb3357685f
Daney <daneyyhh64@gmail.com>
2025-11-04 19:05:23 +0530

    Add kick command placeholder in moderation folder
    This file serves as a placeholder for the kick command, which will allow users to be kicked from the server.

---
+++ commands/moderation/kick.js
@@ -0,0 +1,10 @@
+// Placeholder for kick command
+// This command kicks a user from the server
+
+module.exports = {
+  name: 'kick',
+  description: 'Kick a user from the server',
+  execute(message, args) {
+    // Implementation coming soon
+  },
+};
