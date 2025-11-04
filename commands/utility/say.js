4a0826b51878ad5b9d1ebe707146f7c18c39d7b8
Daney <daneyyhh64@gmail.com>
2025-11-04 19:28:40 +0530

    Add say command to utility

---
+++ commands/utility/say.js
@@ -0,0 +1,8 @@
+// Placeholder for say command
+module.exports = {
+  name: 'say',
+  description: 'Send a message to the channel as the bot',
+  execute(message, args) {
+    // Implementation coming soon
+  },
+};
