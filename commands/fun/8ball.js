6a2bdfdc9640c23e89cb282afe94270f344d8532
Daney <daneyyhh64@gmail.com>
2025-11-04 19:20:45 +0530

    Add placeholder for 8ball command

---
+++ commands/fun/8ball.js
@@ -0,0 +1,8 @@
+// Placeholder for 8ball command
+module.exports = {
+  name: '8ball',
+  description: 'Ask the magic 8-ball a question',
+  execute(message, args) {
+    // Implementation coming soon
+  },
+};
