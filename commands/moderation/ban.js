cd7bb1c4960f4def5560263c595c46afcc625391
Daney <daneyyhh64@gmail.com>
2025-11-04 19:26:30 +0530

    Add ban command to moderation

---
+++ commands/moderation/ban.js
@@ -0,0 +1,8 @@
+// Placeholder for ban command
+module.exports = {
+  name: 'ban',
+  description: 'Ban a user from the server',
+  execute(message, args) {
+    // Implementation coming soon
+  },
+};
